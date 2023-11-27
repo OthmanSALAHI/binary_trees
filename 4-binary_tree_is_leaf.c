0eeb30dfb724c26caa6e351681b61e6a48772552
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-28 00:49:29 +0100

    the leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+*
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+  if (!node)
+    return (0);
+
+  return (!node->left && !node->right) ? (1) : (0);
+
+}
