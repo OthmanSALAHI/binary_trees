73367805ca8b2424fd6639466bb84849e65659e4
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-28 14:23:40 +0100

    number of nodes with children

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+* binary_tree_nodes - counts the node who have child
+* @tree: the tree of nodes
+* Return: number of nodes
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+
+	size_t count = 0;
+
+	if (!tree)
+		return (0);
+
+	count += (tree->left || tree->right) ? 1 : 0;
+	count += (tree->left && tree->right) ? 2 : 0;
+	return (count);
+}
