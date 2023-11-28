36896d9d9e860054202bedd19d6f1cd740e603b2
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-28 23:49:45 +0100

    is the tree is perfect

--- binary_trees.h
+++ binary_trees.h
@@ -63,6 +63,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 
 /* necessaire */
 void binary_tree_print(const binary_tree_t *tree);
