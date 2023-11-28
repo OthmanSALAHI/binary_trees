0e5fca2f4957464d91bbd10e8493ae0dc329ff08
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-28 11:55:05 +0100

    fix the code #11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,13 +6,14 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t count = 0;
+
 	if (!tree)
 		return (0);
 
-	size_t count = 0;
-
 	count += 1;
-	count +=  binary_tree_size(tree->left);
+	count += binary_tree_size(tree->left);
 	count += binary_tree_size(tree->right);
+
 	return (count);
 }
