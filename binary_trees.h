f051485284455a50f65dd0ab7c801a9c22d02ac0
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-29 16:02:19 +0100

    siblings #17

--- binary_trees.h
+++ binary_trees.h
@@ -64,6 +64,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 /* necessaire */
 void binary_tree_print(const binary_tree_t *tree);
