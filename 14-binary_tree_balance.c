319f2824b5af42a81ba2af91d10ef16e7b349133
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-29 16:27:01 +0100

    remove smthing

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,25 +9,25 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree)
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+		return (tree_height(tree->left) - tree_height(tree->right));
 
 	return (0);
 }
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
+ * tree_height - Measures the height of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the height.
  *
  * Return: If tree is NULL, your function must return 0, else return height.
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
 		size_t lysr = 0, lymn = 0;
 
-		lysr = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		lymn = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		lysr = tree->left ? 1 + tree_height(tree->left) : 1;
+		lymn = tree->right ? 1 + tree_height(tree->right) : 1;
 		return ((lysr > lymn) ? lysr : lymn);
 	}
 	return (0);
