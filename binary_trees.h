9c8497cb12dd2842863b1a3153b27be66baec7c9
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-29 18:05:50 +0100

    the level order

--- binary_trees.h
+++ binary_trees.h
@@ -69,7 +69,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second);
-
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+size_t _tree_height(const binary_tree_t *tree);
+void _tree_level(const binary_tree_t *tree, size_t level, void (*func)(int));
 /* necessaire */
 void binary_tree_print(const binary_tree_t *tree);
 
