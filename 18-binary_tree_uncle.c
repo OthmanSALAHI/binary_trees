8fbe4ac1ed449daaef572d88874c55818d949f29
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-29 17:01:54 +0100

    the uncle of node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,13 +8,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
-
-	binary_tree_t *father;
-	binary_tree_t *uncle ;
-
-	father = node->parent;
-	uncle = binary_tree_sibling(father);
-	if (!uncle)
-		return (NULL);
-	return (uncle);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
