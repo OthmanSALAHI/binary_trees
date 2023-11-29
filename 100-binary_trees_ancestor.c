2177ea685569bcae009e8c6ee025910562e53f48
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-29 17:30:02 +0100

    the ancestor of two nodes in my tree

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,11 +10,11 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second)
 {
-	if (!first || !second)
-		return (NULL);
-
 	binary_tree_t *agadir;
 	binary_tree_t *dakhla;
+
+    if (!first || !second)
+		return (NULL);
 
 	if (second == first)
 		return ((binary_tree_t *)second);
