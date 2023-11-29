9e1037f907d0d3f73d8b399ee5d832007c6da3fa
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-29 18:35:39 +0100

    the complet tree with its nodes and leaves

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,48 +1,94 @@
 #include "binary_trees.h"
+#include <stdio.h>
+int wach_wr9a(const binary_tree_t *node);
+size_t _tool(const binary_tree_t *tree);
+int wach_nadia(const binary_tree_t *tree);
+/**
+ * wach_wr9a - checks if a node is a leaf
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, and 0 otherwise. If node is NULL, return 0
+ */
+int wach_wr9a(const binary_tree_t *node)
+{
+	if (node && !node->left && !node->right)
+		return (1);
+	return (0);
+}
 
 /**
- * binary_tree_is_complete - checks if a binary tree is complete
+ * _tool - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height of
+ *
+ * Return: the height of the tree. If tree is NULL, return 0
+ */
+size_t _tool(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (!tree)
+		return (0);
+	left = _tool(tree->left);
+	right = _tool(tree->right);
+	if (left >= right)
+		return (1 + left);
+	return (1 + right);
+}
+
+/**
+ * wach_nadia - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
  *
- * Return: 1 if the tree is complete, 0 otherwise
+ * Return: 1 if perfect, 0 otherwise. If tree is NULL, return 0
+ */
+int wach_nadia(const binary_tree_t *tree)
+{
+	binary_tree_t *l, *r;
+
+	if (!tree)
+		return (1);
+	l = tree->left;
+	r = tree->right;
+	if (wach_wr9a(tree))
+		return (1);
+	if (!l || !r)
+		return (0);
+	if (_tool(l) == _tool(r))
+	{
+		if (wach_nadia(l) && wach_nadia(r))
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_is_complete -  checks if a binary tree is complete
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if complete, 0 otherwise. If tree is NULL, return 0
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
+	size_t l_height, r_height;
+	binary_tree_t *l, *r;
+
 	if (!tree)
 		return (0);
-
-	/* Create a queue for level order traversal */
-	/* Using an array-based queue for simplicity in this example */
-	const binary_tree_t **queue = malloc(sizeof(binary_tree_t *) * 1000);
-	int front = 0, rear = 0;
-
-	queue[rear++] = tree;
-
-	while (front < rear)
+	if (wach_wr9a(tree))
+		return (1);
+	l = tree->left;
+	r = tree->right;
+	l_height = _tool(l);
+	r_height = _tool(r);
+	if (l_height == r_height)
 	{
-		const binary_tree_t *current = queue[front++];
-
-		/* If we encountered a NULL node, break the loop */
-		if (!current)
-			break;
-
-		/* Enqueue left child */
-		queue[rear++] = current->left;
-
-		/* Enqueue right child */
-		queue[rear++] = current->right;
+		if (wach_nadia(l) && binary_tree_is_complete(r))
+			return (1);
 	}
-
-	/* Check if there are any non-NULL nodes after a NULL node */
-	for (; front < rear; front++)
+	else if (l_height == r_height + 1)
 	{
-		if (queue[front] != NULL)
-		{
-			free(queue);
-			return (0);
-		}
+		if (binary_tree_is_complete(l) && wach_nadia(r))
+			return (1);
 	}
-
-	free(queue);
-	return (1);
+	return (0);
 }
