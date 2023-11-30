48aa95f4a926020d273f61ee173ed7b5dbc7f7f1
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-30 16:25:29 +0100

    insert

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,25 +1,23 @@
 #include "binary_trees.h"
 
-size_t height(const binary_tree_t *tree);
 int balance(const binary_tree_t *tree);
 avl_t *avl_insert_recursive(avl_t **tree, avl_t *parent,
 		avl_t **new, int value);
-avl_t *avl_insert(avl_t **tree, int value);
 
 /**
- * height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the height.
+ * _height_ - Measures the _height_ of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the _height_.
  *
- * Return: If tree is NULL, your function must return 0, else return height.
+ * Return: If tree is NULL, your function must return 0, else return _height_.
  */
-size_t height(const binary_tree_t *tree)
+size_t _height_(const binary_tree_t *tree)
 {
 	if (tree != NULL)
 	{
 		size_t l = 0, r = 0;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		l = tree->left ? 1 + binary_tree__height_(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree__height_(tree->right) : 1;
 		return ((l > r) ? l : r);
 	}
 	return (0);
@@ -33,7 +31,7 @@ size_t height(const binary_tree_t *tree)
  */
 int balance(const binary_tree_t *tree)
 {
-	return (tree != NULL ? height(tree->left) - height(tree->right) : 0);
+	return (tree != NULL ? _height_(tree->left) - _height_(tree->right) : 0);
 }
 
 /**
